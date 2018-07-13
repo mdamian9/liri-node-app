b6d90b71b63cbb931530c9bc94bbc507180e2ca2
md_dev <m.damian9@hotmail.com>
2018-07-12 23:19:59 -0700

    do-what-it-says command functions properly, reads file and determines correct command / input

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ var client = new Twitter(Keys.twitter);
 
 // Store user command from process.argv[2] into variable "command"
 var command = process.argv[2];
-var nameOfSearch = process.argv.slice(3).join(" ").trim();
+var userInput = process.argv.slice(3).join(" ").trim();
 
 // This function displays the most recent 20 tweets from the user, and the date of those tweets using the Twitter API
 function myTweets() {
@@ -29,10 +29,10 @@ function myTweets() {
 };
 
 // This function displays information about a song using the Spotify API
-function spotifySong() {
+function spotifySong(songName) {
     // If there was a name of song given, request data for the song inputted by user
-    if (nameOfSearch) {
-        spotify.search({ type: 'track', query: nameOfSearch }, function (error, data) {
+    if (songName) {
+        spotify.search({ type: 'track', query: songName }, function (error, data) {
             if (error) {
                 return console.log('Error occurred: ' + error);
             } else {
@@ -54,10 +54,10 @@ function spotifySong() {
 };
 
 // This function displays information about a movie using the OMDb API
-function movieThis() {
+function movieThis(movieName) {
     // If there was a name of a movie given, request data for the movie inputted by user
-    if (nameOfSearch) {
-        request('http://www.omdbapi.com/?apikey=trilogy&t=' + nameOfSearch, function (error, response, body) {
+    if (movieName) {
+        request('http://www.omdbapi.com/?apikey=trilogy&t=' + movieName, function (error, response, body) {
             var jsonObj = JSON.parse(body);
             console.log("Movie name: " + jsonObj.Title + "\nYear released: " + jsonObj.Year + "\nIMDB rating: " + jsonObj.imdbRating +
                 "\nRotten Tomatoes rating: " + jsonObj.Ratings[1].Value + "\nCountry of production: " + jsonObj.Country + "\nLanguage: " +
@@ -82,17 +82,33 @@ switch (command) {
         break;
     // If command is 'spotify-this-song': call spotifySong function to display song data (will display Billie Jean if no input)
     case "spotify-this-song":
-        spotifySong();
+        spotifySong(userInput)
         break;
     // If command is 'movie-this': call movieThis function to display movie data
     case "movie-this":
-        movieThis();
+        movieThis(userInput);
         break;
+    // If command is 'do-what-it-says': use fs.readFile method to read data from file, and act upon data read
     case "do-what-it-says":
-        console.log("do-what-it-says");
-        // switch (command) {
-        //     case "my-tweets"
-        // }
-
-        break;
+        fs.readFile('random.txt', "utf8", function (error, data) {
+            // Split text into command and input
+            var dataArr = data.split(',');
+            var fileCommand = dataArr[0];
+            var fileInput = dataArr[1];
+            // Switch statement to evaluate what the command in the file is
+            switch (fileCommand) {
+                // If fileCommand is 'my-tweets': call myTweets function to display latest 20 tweets and the date of each tweet
+                case "my-tweets":
+                    myTweets();
+                    break;
+                // If fileCommand is 'spotify-this-song': call spotifySong function to display song data (will display Billie Jean if no input)
+                case "spotify-this-song":
+                    spotifySong(fileInput);
+                    break;
+                // If fileCommand is 'movie-this': call movieThis function to display movie data
+                case "movie-this":
+                    movieThis(fileInput);
+                    break;
+            };
+        });
 };
