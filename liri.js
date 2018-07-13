b2285bcca9508dc4480036bc6f59c16fad5a04fc
md_dev <m.damian9@hotmail.com>
2018-07-12 22:20:26 -0700

    movie-this command functions properly

--- liri.js
+++ liri.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 var Keys = require("./keys.js");
 var Twitter = require("twitter")
 var Spotify = require("node-spotify-api");
-var Request = require("request");
+var request = require("request");
 var fs = require("fs");
 
 // Create variables to hold keys information about Spotify / Twitter
@@ -20,6 +20,7 @@ console.log(nameOfSearch);
 
 // Switch statement that determines what function will run if the case matches the command
 switch (command) {
+    // If command is 'my-tweets': run code, then break
     case "my-tweets":
         var params = { screen_name: '@CarlosM12927691', count: 20 };
         client.get('statuses/user_timeline', params, function (error, tweets, response) {
@@ -32,7 +33,9 @@ switch (command) {
             };
         });
         break;
+    // If command is 'spotify-this-song': run code, then break
     case "spotify-this-song":
+        // If there was a name of song given, request data for the song inputted by user
         if (nameOfSearch) {
             spotify.search({ type: 'track', query: nameOfSearch }, function (error, data) {
                 if (error) {
@@ -42,6 +45,7 @@ switch (command) {
                         "\nPreview link: " + data.tracks.items[0].preview_url + "\nAlbum: " + data.tracks.items[0].album.name);
                 };
             });
+            // If user did not give input for a song, search for Billi Jean by Michael Jackson as default
         } else {
             spotify.search({ type: 'track', query: 'Billie Jean' }, function (error, data) {
                 if (error) {
@@ -53,8 +57,14 @@ switch (command) {
             });
         }
         break;
+    // If command is 'movie-this': run code, then break
     case "movie-this":
-        console.log("movie-this");
+        request('http://www.omdbapi.com/?apikey=trilogy&t=' + nameOfSearch, function (error, response, body) {
+            var jsonObj = JSON.parse(body);
+            console.log("Movie name: " + jsonObj.Title + "\nYear released: " + jsonObj.Year + "\nIMDB rating: " + jsonObj.imdbRating +
+                "\nRotten Tomatoes rating: " + jsonObj.Ratings[1].Value + "\nCountry of production: " + jsonObj.Country + "\nLanguage: " +
+                jsonObj.Language + "\nPlot: " + jsonObj.Plot + "\nActors: " + jsonObj.Actors);
+        });
         break;
     case "do-what-it-says":
         console.log("do-what-it-says");
